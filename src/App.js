de7f94a1243c2ed29d5f47136ce271582080a5a8
Jennie Hellqvist <jennie.hellqvist1@ingka.com>
2022-05-02 20:06:17 +0200

    add ignore

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Jennie Hellqvist</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
